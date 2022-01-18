cf093bfbf097c676f0cd4f11fea64ecfa98df00a
fscarazzato <francesco.scarazzato@gmail.com>
2022-01-18 16:56:15 +0100

    Created first function in Python

--- date.py
+++ date.py
@@ -4,3 +4,11 @@ month = int(date[5:7])
 day = int(date[8:10])
 
 print('Next class will be on', day + 1, '-', month, '-', str(year) + '.') 
+
+def compute_tomorrow(data):
+	year = data[0:4]
+	month = data[5:7]
+	day = int(data[8:10])
+	return 'Tomorrow: ' + year + '-' + month + '-' + str(day + 1)
+
+print(compute_tomorrow('2022-01-17'))
